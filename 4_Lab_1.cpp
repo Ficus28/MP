1bb9b0ce1404ab6d2b47b6c243093b35887d550b
Ficus28 <132055621+Ficus28@users.noreply.github.com>
2024-05-21 15:24:20 +0300

    Add files via upload

---
+++ 4_Lab_1.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+mutex mtx; // мьютекс для защиты вывода в консоль
+condition_variable cv; // условная переменная для синхронизации потоков
+int countA; // счетчик для деталей A
+int countB; // счетчик для деталей B
+int countC ; // счетчик для деталей C
+
+// Функция для производства детали A
+void produceA(int numA) {
+    for (int i = 0; i < numA; ++i) {
+        // Имитация производства детали A
+        this_thread::sleep_for(chrono::seconds(1));
+
+        // Увеличиваем счетчик деталей A
+        unique_lock<mutex> lck(mtx);
+        ++countA;
+        cout << "Деталь A произведена. Прогресс: (" << countA << "/" << numA << ")" << endl;
+        cv.notify_one(); // уведомляем о завершении производства детали A
+    }
+}
+
+// Функция для производства детали B
+void produceB(int numB) {
+    for (int i = 0; i < numB; ++i) {
+        // Имитация производства детали B
+        this_thread::sleep_for(chrono::seconds(2));
+
+        // Увеличиваем счетчик деталей B
+        unique_lock<mutex> lck(mtx);
+        ++countB;
+        cout << "Деталь B произведена. Прогресс: (" << countB << "/" << numB << ")" << endl;
+        cv.notify_one(); // уведомляем о завершении производства детали B
+    }
+}
+
+// Функция для производства детали C
+void produceC(int numC) {
+    for (int i = 0; i < numC; ++i) {
+        // Имитация производства детали C
+        this_thread::sleep_for(chrono::seconds(3));
+
+        // Увеличиваем счетчик деталей C
+        unique_lock<mutex> lck(mtx);
+        ++countC;
+        cout << "Деталь C произведена. Прогресс: (" << countC << "/" << numC << ")" << endl;
+        cv.notify_one(); // уведомляем о завершении производства детали C
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int numA = 9; // количество деталей A
+    int numB = 9; // количество деталей B
+    int numC = 9; // количество деталей C
+
+    thread tA, tB, tC; // потоки для производства деталей A, B и C
+
+    // Запускаем производство каждой детали в отдельных потоках с указанным количеством
+    tA = thread(produceA, numA);
+    tB = thread(produceB, numB);
+    tC = thread(produceC, numC);
+
+    // Ждем завершения производства всех деталей
+    {
+        unique_lock<mutex> lck(mtx);
+        cv.wait(lck, [&] { return countA >= numA && countB >= numB && countC >= numC; });
+    }
+
+    // Выводим сообщение о завершении производства всех деталей
+    cout << "Все детали произведены. Сборка винтика завершена." << endl;
+
+    // Дожидаемся завершения всех потоков
+    tA.join();
+    tB.join();
+    tC.join();
+
+    return 0;
+}
---
+++ 4_Lab_3.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+#include <chrono>
+
+class MessageQueue {
+private:
+    std::queue<std::string> queue;
+    mutable std::mutex mutex;
+    std::condition_variable cv;
+    const size_t maxQueueSize = 20;
+    bool isDropped = false;
+    int numProducers = 0;
+    int numConsumers = 0;
+
+public:
+    void put(const std::string& msg) {
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [this]() { return queue.size() < maxQueueSize || isDropped; });
+            ++numProducers;
+        }
+
+        if (!isDropped) {
+            std::string truncatedMsg = msg.substr(0, 80); // Сократить до 80 символов
+            {
+                std::lock_guard<std::mutex> lock(mutex);
+                queue.push(truncatedMsg);
+                std::cout << "Отправленное сообщение: " << truncatedMsg << std::endl;
+            }
+            cv.notify_one();
+        }
+    }
+
+    std::string get() {
+        std::string msg;
+
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [this]() { return !queue.empty() || isDropped; });
+            ++numConsumers;
+            if (!isDropped) {
+                msg = queue.front();
+                queue.pop();
+                std::cout << "Получение сообщения: " << msg << std::endl;
+            }
+        }
+
+        if (numConsumers >= 2) {
+            cv.notify_one();
+            numConsumers = 0;
+        }
+
+        return msg;
+    }
+
+    void drop() {
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            isDropped = true;
+        }
+        cv.notify_all();
+    }
+
+    bool empty() const {
+        std::lock_guard<std::mutex> lock(mutex);
+        return queue.empty();
+    }
+};
+
+void producer(MessageQueue& mq, int id) {
+    for (int i = 0; i < 15; ++i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Задержка для имитации работы
+        mq.put("Сообщение от производителя " + std::to_string(id) + ", номер " + std::to_string(i));
+    }
+}
+
+void consumer(MessageQueue& mq, int id) {
+    while (true) {
+        std::string msg = mq.get();
+        if (msg.empty()) break; // Завершить цикл, если очередь удалена
+        std::this_thread::sleep_for(std::chrono::milliseconds(800)); // Задержка для имитации работы
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    MessageQueue mq;
+
+    std::thread producer1(producer, std::ref(mq), 1);
+    std::thread producer2(producer, std::ref(mq), 2);
+    std::thread consumer1(consumer, std::ref(mq), 1);
+    std::thread consumer2(consumer, std::ref(mq), 2);
+
+    producer1.join();
+    producer2.join();
+
+    mq.drop(); // Удалить очередь после завершения работы производителей
+
+    consumer1.join();
+    consumer2.join();
+
+    return 0;
+}
